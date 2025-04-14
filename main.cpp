e7760a424803957dfae0984c874f995f1904ebc8
Viktor Khasenko <developer@viktorkhasenko.com>
2025-04-14 22:37:04 +0100

    Based textures

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "raylib.h"
+
+int main() {
+    // Initialization
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "Jump And Dash Game"); // Create window and OpenGL context
+
+    SetTargetFPS(60); // Set our game to run at 60 frames-per-second
+
+    // Main game loop
+    while (!WindowShouldClose()) { // Detect window close button or ESC key
+        // Update
+        // Draw
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+
+        DrawText("Welcome to the Jump And Dash Game!", 190, 200, 20, LIGHTGRAY);
+        DrawText("Press ESC to exit", 290, 240, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    // De-Initialization
+    CloseWindow(); // Close window and OpenGL context
+
+    return 0;
+}
