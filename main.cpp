a0a1dc05f28fd26640a18165f30ed1c32a8397ff
Viktor Khasenko <developer@viktorkhasenko.com>
2025-04-16 16:11:25 +0100

    Refactor animation logic; update character frame handling and running time management

--- main.cpp
+++ main.cpp
@@ -18,12 +18,19 @@ int main() {
     characterPos.x = windowWidth / 2 - characterRec.width / 2; // Center the character
     characterPos.y = windowHeight - characterRec.height; // Set y position to the bottom of the window
 
-    // Character dimensions
-
+    // Animation frame
+    int frame{0}; // Frame counter
     int velocity{0}; // Initialize velocity
     bool isInAir{false}; // Jumping state
     const int jumpVelocity{-600}; // Jump velocity (pixels/second)
     const int dashVelocity{-800}; // Dash velocity (pixels/second)
+
+    // Amount of time to wait before updating the animation frame
+    // This is used to control the speed of the animation
+    // The value is set to 1/12 of a second, which means the animation will update every 12 frames
+    // This is a common frame rate for animations, as it gives a smooth look without being too fast
+    const float updateTime{1.0/12.0}; // Update time (seconds)
+    float runningTime{0}; // Running time (seconds)
 
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
 
@@ -39,7 +46,6 @@ int main() {
         DrawText("Press SPACE to jump", 10, 10, 20, DARKGRAY);
         DrawText("Press D to dash", 10, 30, 20, DARKGRAY);
         DrawText("Press ESC to exit", 10, 50, 20, DARKGRAY);
-
 
         // Perform ground collision detection
         if (characterPos.y < windowHeight - characterRec.height) {
@@ -61,7 +67,19 @@ int main() {
             velocity = dashVelocity; // Dash
         }
 
-        characterPos.y += velocity * deltaTime; // Update the position
+        // Update the position
+        characterPos.y += velocity * deltaTime; 
+
+        runningTime += deltaTime; // Update running time
+        if (runningTime >= updateTime) { // Check if it's time to update the animation frame
+            runningTime = 0; // Reset running time
+            
+            //Update animation frame
+            characterRec.x = frame * characterRec.width; // Set the x position of the texture
+ 
+            frame++; // Update frame
+            if (frame > 5) frame = 0; // Reset frame counter
+        }
 
         EndDrawing();
     }
