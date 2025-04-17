e3b51d58679456651e6f58d53242ea5400b39e8b
Viktor Khasenko <developer@viktorkhasenko.com>
2025-04-17 20:27:20 +0100

    Refactor animation data structure; consolidate nebula and character animation properties

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include "raylib.h"
 
+struct AnimationData {
+    Rectangle rec; // Rectangle for the frame
+    Vector2 pos; // Position of the frame
+    int frame; // Frame counter
+    float runningTime{0.0f}; // Running time (seconds)
+    float updateTime{1.0f/12.0f}; // Update time (seconds)
+};
+
 int main() {
     // Initialization
     const int windowWidth = 800;
@@ -11,38 +19,38 @@ int main() {
  
     // Nebula 
     Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png"); // Load nebula texture
-    Rectangle nebulaRec{0.0f, 0.0f, nebula.width/8.0f, nebula.height/8.0f}; // Nebula rectangle
-    Vector2 nebulaPos{windowWidth, windowHeight - nebulaRec.height }; // Nebula position
 
+    AnimationData nebulaData{ 
+        {0.0f, 0.0f, nebula.width/8.0f, nebula.height/8.0f}, // Rectangle for the frame
+        {windowWidth, windowHeight - nebula.height/8.0f}, // Position of the frame
+        0, // Frame counter
+        0.0f, // Running time (seconds)
+        1.0f/12.0f // Update time (seconds)
+    }; // Nebula animation data
 
-    // Nebula 2
-    Rectangle nebula2Rec{0.0f, 0.0f, nebula.width/8.0f, nebula.height/8.0f}; // Nebula 2 rectangle
-    Vector2 nebula2Pos{windowWidth + 400, windowHeight - nebulaRec.height }; // Nebula 2 position
-    
-    // Nebula Animation
-    int nebulaFrame{0}; // Nebula frame counter
-    const float nebulaUpdateTime{1.0/12.0}; // Update time (seconds)
-    float nebulaRunningTime{0}; // Nebula running time (seconds)
-    
-
-    // Nebula 2 Animation
-    int nebula2Frame{0};
-    const float nebula2UpdateTime{1.0/16.0}; // Update time (seconds)
-    float nebula2RunningTime{0}; // Nebula 2 running time (seconds)
+    AnimationData nebula2Data{ 
+        {0.0f, 0.0f, nebula.width/8.0f, nebula.height/8.0f}, // Rectangle for the frame
+        {windowWidth + 400, windowHeight - nebula.height/8.0f}, // Position of the frame
+        0, // Frame counter
+        0.0f, // Running time (seconds)
+        1.0f/16.0f // Update time (seconds)
+    }; // Nebula 2 animation data
 
     // Set nebula X velocity (pixels/second) 
     int nebulaVelocity{-200}; // Nebula velocity 
 
     // Main Character  
     Texture2D character = LoadTexture("textures/scarfy.png"); // Load character texture
-    Rectangle characterRec; // Character rectangle
-    characterRec.width = character.width/6; // Set width
-    characterRec.height = character.height; // Set height
-    characterRec.x = 0; // Set x position
-    characterRec.y = 0; // Set y position
-    Vector2 characterPos; // Character position
-    characterPos.x = windowWidth / 2 - characterRec.width / 2; // Center the character
-    characterPos.y = windowHeight - characterRec.height; // Set y position to the bottom of the window
+    AnimationData characterData; // Character animation data
+    characterData.rec.width = character.width/6; // Set width
+    characterData.rec.height = character.height; // Set height
+    characterData.rec.x = 0; // Set x position
+    characterData.rec.y = 0; // Set y position
+    characterData.pos.x = windowWidth / 2 - characterData.rec.width / 2; // Center the character
+    characterData.pos.y = windowHeight - characterData.rec.height; // Set y position to the bottom of the window
+    characterData.frame = 0; // Initialize frame counter
+    characterData.runningTime = 0.0f; // Initialize running time
+    characterData.updateTime = 1.0f/12.0f; // Set update time (seconds)
 
     // Animation frame
     int frame{0}; // Frame counter
@@ -69,7 +77,7 @@ int main() {
         ClearBackground(RAYWHITE);
         
         // Perform ground collision detection
-        if (characterPos.y < windowHeight - characterRec.height) {
+        if (characterData.pos.y < windowHeight - characterData.rec.height) {
             // Rectagle is oin air
             isInAir = true;
             velocity += gravity * deltaTime; // Apply gravity
@@ -77,7 +85,7 @@ int main() {
             // Rectagle is on the ground
             isInAir = false;
             velocity = 0; // Reset velocity when on the ground
-            characterPos.y = windowHeight - characterRec.height; // Reset position to ground level
+            characterData.pos.y = windowHeight - characterData.rec.height; // Reset position to ground level
         }
 
         // jump and dash logic
@@ -89,12 +97,12 @@ int main() {
         }
 
         // Move nebula position
-        nebulaPos.x += nebulaVelocity * deltaTime; // Update nebula position
+        nebulaData.pos.x += nebulaVelocity * deltaTime; // Update nebula position
         // Move nebula 2 position
-        nebula2Pos.x += nebulaVelocity * deltaTime; // Update nebula 2 position
+        nebula2Data.pos.x += nebulaVelocity * deltaTime; // Update nebula 2 position
         
         // Update character the position
-        characterPos.y += velocity * deltaTime; 
+        characterData.pos.y += velocity * deltaTime; 
 
         if(!isInAir) {
             // Update running time 
@@ -102,7 +110,7 @@ int main() {
             if (runningTime >= updateTime) { // Check if it's time to update the animation frame
                 runningTime = 0; // Reset running time
                 //Update animation frame
-                characterRec.x = frame * characterRec.width; // Set the x position of the texture
+                characterData.rec.x = frame * characterData.rec.width; // Set the x position of the texture
                 // Update frame
                 frame++; 
                 // Reset frame counter
@@ -111,38 +119,37 @@ int main() {
         } 
 
         // Update nebula animation frame
-        nebulaRunningTime += deltaTime; // Update running time
-        if (nebulaRunningTime >= nebulaUpdateTime) { // Check if it's time to update the animation frame
-            nebulaRunningTime = 0; // Reset running time
+        nebulaData.runningTime += deltaTime; // Update running time
+        if (nebulaData.runningTime >= nebulaData.updateTime) { // Check if it's time to update the animation frame
+            nebulaData.runningTime = 0; // Reset running time
             // Update nebula frame
-            nebulaRec.x = nebulaFrame * nebulaRec.width; // Set the x position of the texture
+            nebulaData.rec.x = nebulaData.frame * nebulaData.rec.width; // Set the x position of the texture
             // Update frame
-            nebulaFrame++; 
+            nebulaData.frame++; 
             // Reset frame counter
-            if (nebulaFrame > 7) nebulaFrame = 0; 
+            if (nebulaData.frame > 7) nebulaData.frame = 0; 
         }
 
 
         // Update nebula animation frame
-        nebula2RunningTime += deltaTime; // Update running time
-        if (nebula2RunningTime >= nebula2UpdateTime) { // Check if it's time to update the animation frame
-            nebula2RunningTime = 0; // Reset running time
+        nebula2Data.runningTime += deltaTime; // Update running time
+        if (nebula2Data.runningTime >= nebula2Data.updateTime) { // Check if it's time to update the animation frame
+            nebula2Data.runningTime = 0; // Reset running time
             // Update nebula frame
-            nebula2Rec.x = nebula2Frame * nebula2Rec.width; // Set the x position of the texture
+            nebula2Data.rec.x = nebula2Data.frame * nebula2Data.rec.width; // Set the x position of the texture
             // Update frame
-            nebula2Frame++; 
+            nebula2Data.frame++; 
             // Reset frame counter
-            if (nebula2Frame > 7) nebula2Frame = 0; 
+            if (nebula2Data.frame > 7) nebula2Data.frame = 0; 
         }
 
         // Draw nebula
-        DrawTextureRec(nebula, nebulaRec, nebulaPos, WHITE);
+        DrawTextureRec(nebula, nebulaData.rec, nebulaData.pos, WHITE);
         // Draw nebula 2
-        DrawTextureRec(nebula, nebula2Rec, nebula2Pos, RED);
-
+        DrawTextureRec(nebula, nebula2Data.rec, nebula2Data.pos, RED);
 
         // Draw character
-        DrawTextureRec(character, characterRec, characterPos, WHITE); 
+        DrawTextureRec(character, characterData.rec, characterData.pos, WHITE); 
         
         EndDrawing();
     }
