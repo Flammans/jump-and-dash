976f91f485720b2eda3b5e171afecb24d8cb3e84
Viktor Khasenko <developer@viktorkhasenko.com>
2025-04-18 22:38:51 +0100

    Refactor code structure for improved readability and maintainability

--- main.cpp
+++ main.cpp
@@ -43,6 +43,7 @@ void DrawParallaxLayer(Texture2D texture, float &positionX, float speed, float d
     for (int i = 0; i < count; i++)
     {
         Vector2 position = { positionX + texture.width * scale * i, 0.0f };
+        // Draw the texture
         DrawTextureEx(texture, position, 0.0f, scale, WHITE);
     }
 }
@@ -54,34 +55,44 @@ int main() {
     const int windowWidth = 1200;
     const int windowHeight = 720;
 
-    InitWindow(windowWidth, windowHeight, "Jump And Dash Game"); // Create window and OpenGL context
+    InitWindow(windowWidth, windowHeight, "Dnipro Roller"); // Create window and OpenGL context
 
     const int gravity{1'000}; // Aceleration due to gravity (pixels/second)/second
  
-    // Nebula 
-    Texture2D nebula = LoadTexture("textures/12_nebula_spritesheet.png"); // Load nebula texture
+    // Czech-Hedgehog 
+    Texture2D czechHedgehog = LoadTexture("textures/czech-hedgehog.png"); // Load czechHedgehog texture
 
-    const int sizeOfNebula{3}; // Number of frames in the nebula texture
-    AnimationData nebulae[sizeOfNebula]{}; // Array of nebulae
+    const int sizeOfczechHedgehog{3}; // Number of frames in the czechHedgehog texture
+    AnimationData czechHedgehogs[sizeOfczechHedgehog]{}; // Array of czechHedgehoge
 
-    for (int i = 0; i < sizeOfNebula; i++)
+    for (int i = 0; i < sizeOfczechHedgehog; i++)
     {
-        nebulae[i].rec.x = 0.0f; // Set x position
-        nebulae[i].rec.y = 0.0f; // Set y position
-        nebulae[i].rec.width = nebula.width/8.0f; // Set width
-        nebulae[i].rec.height = nebula.height/8.0f; // Set height
-        nebulae[i].pos.x = windowWidth + GetRandomValue(0, 1200); // Set x position
-        nebulae[i].pos.y = windowHeight - nebula.height/8.0f; // Set y position
-        nebulae[i].frame = 0; // Initialize frame counter
-        nebulae[i].runningTime = 0.0f; // Initialize running time
-        nebulae[i].updateTime = 1.0f/16.0f; // Set update time (seconds)
+        czechHedgehogs[i].rec.x = 0.0f; // Set x position
+        czechHedgehogs[i].rec.y = 0.0f; // Set y position
+        czechHedgehogs[i].rec.width = czechHedgehog.width; // Set width
+        czechHedgehogs[i].rec.height = czechHedgehog.height; // Set height
+        czechHedgehogs[i].pos.x = windowWidth + GetRandomValue(0, 1200); // Set x position
+        czechHedgehogs[i].pos.y = windowHeight - czechHedgehog.height; // Set y position
+        czechHedgehogs[i].frame = 0; // Initialize frame counter
+        czechHedgehogs[i].runningTime = 0.0f; // Initialize running time
+        czechHedgehogs[i].updateTime = 1.0f/16.0f; // Set update time (seconds)
     }
 
-    // Set nebula X velocity (pixels/second) 
-    int nebulaVelocity{-200}; // Nebula velocity 
+    
+    // Set czechHedgehog X velocity (pixels/second) 
+    int czechHedgehogVelocity{-180}; // czechHedgehog velocity 
+
+    // Finish Line
+    Texture2D finishLine = LoadTexture("textures/finish-line.png"); // Load finish line texture
+    Rectangle finishLineRec; // Finish line rectangle
+    Vector2 finishLinePos; // Finish line position
+    finishLineRec.x = 0; // Set x position
+    finishLineRec.y = 0; // Set y position
+    finishLineRec.width = finishLine.width; // Set width
+    finishLineRec.height = finishLine.height; // Set height
 
     // Main Character  
-    Texture2D character = LoadTexture("textures/scarfy.png"); // Load character texture
+    Texture2D character = LoadTexture("textures/rollfy.png"); // Load character texture
     AnimationData characterData; // Character animation data
     characterData.rec.width = character.width/6; // Set width
     characterData.rec.height = character.height; // Set height
@@ -91,7 +102,7 @@ int main() {
     characterData.pos.y = windowHeight - characterData.rec.height; // Set y position to the bottom of the window
     characterData.frame = 0; // Initialize frame counter
     characterData.runningTime = 0.0f; // Initialize running time
-    characterData.updateTime = 1.0f/12.0f; // Set update time (seconds)
+    characterData.updateTime = 10.0f/12.0f; // Set update time (seconds)
 
     // Animation frame
     int velocity{0}; // Initialize velocity
@@ -99,16 +110,13 @@ int main() {
     const int jumpVelocity{-600}; // Jump velocity (pixels/second)
     const int dashVelocity{-800}; // Dash velocity (pixels/second)
 
-    Texture2D background = LoadTexture("textures/far-buildings.png"); // Load background texture
-    Texture2D midground = LoadTexture("textures/back-buildings.png"); // Load midground texture
-    Texture2D foreground = LoadTexture("textures/foreground.png"); // Load foreground texture
+    Texture2D background = LoadTexture("textures/dnipro-far-buildings.png"); // Load background texture
+    Texture2D foreground = LoadTexture("textures/dnipro-street.png"); // Load foreground texture
     
     float bagroundPositionX{0.0f}; // Background position
-    float migroundPositionX{0.0f}; // Midground position
     float foregroundPositionX{0.0f}; // Foreground position
 
     int backgroundCount{3}; // Background layer count
-    int midgroundCount{3}; // Midground layer count
     int foregroundCount{3}; // Foreground layer count
 
     SetTargetFPS(60); // Set our game to run at 60 frames-per-second
@@ -117,9 +125,8 @@ int main() {
     while (!WindowShouldClose()) { // Detect window close button or ESC key
         // Update
         float deltaTime{GetFrameTime()}; // Get time between frames
-        DrawParallaxLayer(background, bagroundPositionX, 20.0f, deltaTime, 4.0f, backgroundCount); // Draw background
-        DrawParallaxLayer(midground, migroundPositionX, 40.0f, deltaTime, 4.0f, midgroundCount); // Draw midground
-        DrawParallaxLayer(foreground, foregroundPositionX, 80.0f, deltaTime, 4.0f, foregroundCount); // Draw foreground
+        DrawParallaxLayer(background, bagroundPositionX, 10.0f, deltaTime, 0.75f, backgroundCount); // Draw background
+        DrawParallaxLayer(foreground, foregroundPositionX, 180.0f, deltaTime, 0.75, foregroundCount); // Draw foreground
         
         // Perform ground collision detection
         if (IsOnGround(characterData, windowHeight)) {
@@ -141,10 +148,10 @@ int main() {
             velocity = dashVelocity; // Dash
         }
 
-        for (int i = 0; i < sizeOfNebula; i++)
+        for (int i = 0; i < sizeOfczechHedgehog; i++)
         {
-            // Move nebula position
-            nebulae[i].pos.x += nebulaVelocity * deltaTime; // Update nebula position
+            // Move czechHedgehog position
+            czechHedgehogs[i].pos.x += czechHedgehogVelocity * deltaTime; // Update czechHedgehog position
         }
 
         // Update character the position
@@ -154,30 +161,32 @@ int main() {
             characterData = updateAnimationData(characterData, deltaTime, 5); // Update character animation
         } 
 
-        for (int i = 0; i < sizeOfNebula; i++)
+        for (int i = 0; i < sizeOfczechHedgehog; i++)
         {
-            // Check if nebula is out of the screen
-            if (nebulae[i].pos.x < -nebulae[i].rec.width) {
-                nebulae[i].pos.x = windowWidth + GetRandomValue(0, 400); // Reset position to the right side of the screen
+            // Check if czechHedgehog is out of the screen
+            if (czechHedgehogs[i].pos.x < -czechHedgehogs[i].rec.width) {
+                czechHedgehogs[i].pos.x = windowWidth + GetRandomValue(0, 400); // Reset position to the right side of the screen
             }
  
-            nebulae[i] = updateAnimationData(nebulae[i], deltaTime, 7); // Update nebula animation
+            czechHedgehogs[i] = updateAnimationData(czechHedgehogs[i], deltaTime, 7); // Update czechHedgehog animation
 
-            DrawTextureRec(nebula, nebulae[i].rec, nebulae[i].pos, WHITE); // Draw nebula
+            DrawTextureRec(czechHedgehog, czechHedgehogs[i].rec, czechHedgehogs[i].pos, WHITE); // Draw czechHedgehog
+  
+            
         }
 
         // Draw character
         DrawTextureRec(character, characterData.rec, characterData.pos, WHITE); 
-        
+
         EndDrawing();
     }
 
     // De-Initialization
     UnloadTexture(character); // Unload texture
-    UnloadTexture(nebula); // Unload texture
+    UnloadTexture(czechHedgehog); // Unload texture
     UnloadTexture(background); // Unload texture
-    UnloadTexture(midground); // Unload texture
     UnloadTexture(foreground); // Unload texture
+    UnloadTexture(finishLine); // Unload texture
     CloseWindow(); // Close window and OpenGL context
 
     return 0;
